0ecff03f53bae5445168733e6ab12fb9aa538625
Mahmoud Gad <99031876+mega404@users.noreply.github.com>
2022-12-21 13:44:29 +0200

    ss

---
+++ Client.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+
+#define PORT "8082" // the port client will be connecting to
+
+#define MAXDATASIZE 1000000 // max number of bytes we can get at once
+#define END "\\r\\n\n"
+#define MAXINPUTLINES 100
+void write_file(char *path, char *data);
+void get_file_data(int sockfd, char *buf);
+
+// get sockaddr, IPv4 or IPv6:
+void* get_in_addr(struct sockaddr *sa) {
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*) sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*) sa)->sin6_addr);
+}
+
+int main(int argc, char *argv[]) {
+	int sockfd, numbytes;
+	char buf[MAXDATASIZE];
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET6_ADDRSTRLEN];
+	char *command = NULL;
+	size_t len = 0;
+	ssize_t read = 0;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: client hostname\n");
+		exit(1);
+	}
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and connect to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))
+				== -1) {
+			perror("client: socket");
+			continue;
+		}
+
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("client: connect");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "client: failed to connect\n");
+		return 2;
+	}
+
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr*) p->ai_addr), s,
+			sizeof s);
+	printf("client: connecting to %s\n", s);
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	while (1) {
+		char input[2048];
+		memset(input, '\0', 2048 * sizeof(char));
+		printf("Enter your command:\n");
+		do {
+			read = getline(&command, &len, stdin);
+			if (read == -1)
+				return -1;
+			command[read] = '\0';
+			strcat(input, command);
+		} while (strcmp(command, END) != 0);
+
+		printf("Completed input\n");
+		if (send(sockfd, input, strlen(input), 0) == -1) {
+			perror("send");
+			break;
+		}
+		printf("Sent request\n");
+
+		if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1) {
+			perror("recv");
+			break;
+		}
+		buf[numbytes] = '\0';
+		get_file_data(sockfd, buf);
+		printf("client: received '%s'\n", buf);
+	}
+	close(sockfd);
+	return 0;
+}
+
+void get_file_data(int sockfd, char *buf) {
+	printf("Reading Picture Byte Array\n");
+	FILE *fileptr;
+	fileptr = fopen("1.png", "w");
+
+	if (fileptr == NULL) {
+		printf("unable to create file ");
+		exit(EXIT_FAILURE);
+	}
+
+	int size = 100000;
+	char p_array[size];
+	char *current = p_array;
+	int nb = read(sockfd, current, size);
+	while (nb >= 0) {
+		current = current + nb;
+		nb = read(sockfd, current, size);
+		printf("%s ", current);
+		fflush(stdout);
+		/*fputs(current, fileptr);*/
+	}
+	fclose(fileptr);
+}
+
+void write_file(char *path, char *data) {
+	FILE *fileptr;
+	fileptr = fopen(path, "w");
+
+	if (fileptr == NULL) {
+		printf("unable to create file ");
+		exit(EXIT_FAILURE);
+	}
+
+	fputs(data, fileptr);
+	fclose(fileptr);
+
+}
+
+//tests
+//POST C:\Users\SourcesNet\Desktop\assigment2_test.txt http1.1 this_is_the_writen_code
+//GET C:\Users\SourcesNet\Desktop\assigment2_test.txt http1.1
---
+++ Server.c
@@ -0,0 +1,303 @@
+//
+// Created by SourcesNet on 12/21/2022.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PORT "8082"  // the port users will be connecting to
+
+#define BACKLOG 10   // how many pending connections queue will hold
+#define MAXDATASIZE 2048 // max number of bytes we can get at once
+#define MAX_COMMANDS_SPACES 100
+#define END "\\r\\n\n"
+
+char Notfound[] = "HTTP/1.1 404 Not Found\\r\\n";
+char Ok[] = "HTTP/1.1 200 OK\\r\\n";
+
+void get_command(int new_fd, char buffer[2048]);
+void parse_command(int new_fd, char *buf, char (*parsed)[1024], char *response);
+void handle_get(int new_fd, char *path, char *response);
+void read_file(int new_fd, char *path, char *reponse);
+
+void write_file(char path[1024], char data[1024]);
+
+void sigchld_handler(int s) {
+	// waitpid() might overwrite errno, so we save and restore it:
+	int saved_errno = errno;
+
+	while (waitpid(-1, NULL, WNOHANG) > 0)
+		;
+
+	errno = saved_errno;
+}
+
+// get sockaddr, IPv4 or IPv6:
+void* get_in_addr(struct sockaddr *sa) {
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*) sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*) sa)->sin6_addr);
+}
+
+int main(void) {
+	char buf[MAXDATASIZE];
+	char parsed[MAX_COMMANDS_SPACES][1024];
+	int sockfd, new_fd, numbytes; // listen on sock_fd, new connection on new_fd
+//
+//	int shm_id;
+//	key_t mem_key;
+//	int *numOfConnections;
+//	mem_key = ftok(".", 'a');
+//	shm_id = shmget(mem_key, sizeof(int), IPC_CREAT | 0666);
+//	if (shm_id < 0) {
+//		printf("*** shmget error (server) ***\n");
+//		exit(1);
+//	}
+//	numOfConnections = (int*)  shmat(shm_id, NULL, 0);
+//	/* attach */
+//	if (*numOfConnections == -1) {
+//		printf("*** shmat error (server) ***\n");
+//		exit(1);
+//	}
+
+	int *numOfConnections = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE,
+	MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes = 1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))
+				== -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))
+				== -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		const int enable = 1;
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int))
+				< 0)
+			printf("setsockopt(SO_REUSEADDR) failed");
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+	printf("server: waiting for connections...\n");
+
+	while (1) {  // main accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr*) &their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+		inet_ntop(their_addr.ss_family,
+				get_in_addr((struct sockaddr*) &their_addr), s, sizeof s);
+		printf("server: got connection from %s\n", s);
+
+		if (!fork()) { // this is the child process
+			close(sockfd); // child doesn't need the listener
+			while (1) {
+				fd_set readfds;
+				struct timeval tv;
+
+				FD_ZERO(&readfds);
+
+				FD_SET(new_fd, &readfds);
+				tv.tv_sec = 1000;
+
+				rv = select(new_fd + 1, &readfds, NULL, NULL, &tv);
+
+				if (rv == -1) {
+					perror("select"); // error occurred in select()
+				} else if (rv == 0) {
+					printf("Timeout occurred! No data after 100 seconds.\n");
+					break;
+				}
+
+				char buffer[2048];
+				char response[1000000];
+				memset(response, '\0', 1000000 * sizeof(char));
+				get_command(new_fd, buffer);
+				printf("Server: received '%s'\n", buffer);
+				parse_command(new_fd, buffer, parsed, response);
+				if (send(new_fd, response, strlen(response), 0) == -1)
+					perror("send");
+				if (strcmp(response, "CLOSE") == 0) {
+					break;
+				}
+			}
+			printf("Closing...\n");
+			fflush(stdout);
+			close(new_fd);
+			exit(0);
+		}
+		close(new_fd);  // parent doesn't need this
+	}
+
+	return 0;
+}
+
+void get_command(int new_fd, char buffer[2048]) {
+	int numbytes;
+	int i = 0;
+	char command[2048];
+	memset(command, '\0', 2048 * sizeof(char));
+	if ((numbytes = recv(new_fd, command, 2048 - 1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+
+	printf("The received command is %s", command);
+	strcpy(buffer, command);
+
+	command[numbytes] = '\0';
+	printf("received request\n");
+	fflush(stdout);
+}
+
+void parse_command(int new_fd, char *buf, char (*parsed)[1024], char *response) {
+	char *token;
+	char *rest = buf;
+	int i = 0;
+
+	while ((token = strtok_r(rest, " ", &rest))) {
+		strcpy(parsed[i], token);
+		i++;
+	}
+
+	if (strcmp(parsed[0], "GET") == 0) {
+		handle_get(new_fd, parsed[1], response);
+	} else if (strcmp(parsed[0], "POST") == 0) {
+		write_file(parsed[1], parsed[3]);
+		strcpy(response, Ok);
+	} else if (strcmp(parsed[0], "CLOSE") == 0) {
+		strcpy(response, "CLOSE");
+	} else {
+		strcpy(response, "UNKNOWN REQUEST");
+	}
+}
+
+void write_file(char path[1024], char data[1024]) {
+	FILE *fileptr;
+	fileptr = fopen(path, "w");
+
+	if (fileptr == NULL) {
+		printf("unable to create file ");
+		exit(EXIT_FAILURE);
+	}
+
+	fputs(data, fileptr);
+	fclose(fileptr);
+
+}
+
+void handle_get(int new_fd, char *path, char *response) {
+	if (access(path, F_OK) == 0) {
+		// file exists
+		read_file(new_fd, path, response);
+	} else {
+		strcpy(response, Notfound);
+	}
+}
+
+void read_file(int new_fd, char *path, char *response) {
+//	printf("in read file\n");
+//	fflush(stdout);
+//	FILE *fileptr;
+//	char *buffer;
+//	long filelen;
+//	fileptr = fopen(path, "rb");  // Open the file in binary mode
+//	fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
+//	filelen = ftell(fileptr);         // Get the current byte offset in the file
+//	rewind(fileptr);                   // Jump back to the beginning of the file
+//	buffer = (char*) malloc(filelen * sizeof(char)); // Enough memory for the file
+//	fread(buffer, filelen, 1, fileptr); // Read in the entire file
+//	fclose(fileptr); // Close the file
+//	char * modifiedBuffer = (char*) malloc( (200 + filelen) * sizeof(char)); // Enough memory for the file
+//	strcpy(modifiedBuffer, Ok);
+//	strcat(modifiedBuffer, buffer);
+//	strcpy(response, modifiedBuffer);
+//	printf("\nfinal buffer is %s", response);
+//	fflush(stdout);
+//	free(buffer);
+//	free(modifiedBuffer);
+	FILE *fileptr;
+	long filelen;
+	fileptr = fopen(path, "rb");  // Open the file in binary mode
+	fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
+	filelen = ftell(fileptr);         // Get the current byte offset in the file
+	rewind(fileptr);
+	char send_buffer[100000]; // no link between BUFSIZE and the file size
+	int nb = fread(send_buffer, 1, sizeof(send_buffer), fileptr);
+	while (!feof(fileptr)) {
+		write(new_fd, send_buffer, nb);
+		nb = fread(send_buffer, 1, sizeof(send_buffer), fileptr);
+		// no need to bzero
+	}
+}
